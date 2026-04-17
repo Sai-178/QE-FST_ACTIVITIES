5c19c5e3184aeb757ec13c1ec3de192ce6cbd9eb
Sai-178 <sainarendra178@gmail.com>
2026-04-17 13:37:09 +0530

    Fix variable name typo for WebElement in Act17.java

--- Java-Selenium/Act17.java
+++ Java-Selenium/Act17.java
@@ -13,9 +13,9 @@ public class Act17 {
     public static void main(String[] args) {
         WebDriver driver = new FirefoxDriver();
         driver.get("https://training-support.net/webelements/selects/");
-        WebElement wel = driver.findElement(By.xpath("//select[@class='h-80 w-64 rounded-lg border-2 border-black bg-purple-200 p-3 shadow-md transition hover:shadow-lg focus:border-2 focus:border-black focus:ring-0']"));
+        WebElement well = driver.findElement(By.xpath("//select[@class='h-80 w-64 rounded-lg border-2 border-black bg-purple-200 p-3 shadow-md transition hover:shadow-lg focus:border-2 focus:border-black focus:ring-0']"));
 
-        Select sel = new Select(wel);
+        Select sel = new Select(well);
         sel.selectByIndex(1);
         sel.selectByValue("html");
         sel.selectByVisibleText("CSS");
